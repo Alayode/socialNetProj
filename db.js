6bd75de752341a53dface2e88a7ec7dc6d694757
chris <ksamuel.chris@gmail.com>
2015-04-29 17:46:25 -0400

    fixed database url to work locally post shows in collections

--- db.js
+++ db.js
@@ -6,11 +6,19 @@ $ npm install --save mongoose
 
 It is better to keep this code modularized so your server.js does not get too large.
 */
+//
+// var mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost/social',function(){
+//   console.log('mongodb connected')
+// })
+//
+//
+// module.exports = mongoose
+
 
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/social',function(){
+var url = process.env.MONGODB_URL || process.env.MONGOLAB_URI || 'localhost:27017/test'
+mongoose.connect(url, function () {
   console.log('mongodb connected')
 })
-
-
 module.exports = mongoose
--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var Post = require('./models/post')
 
 
 var app = express()
 app.use(bodyParser.json())
 
-app.get('/', function(req,res){
-  res.sendfile('layouts/posts.html')
-})
 
-
-//
 app.get('/api/posts', function(req,res){
   res.json([
     {
       username:'battlerUshinomiya',
-      body:'畜生おおぉぉ！'
+      body:'畜生おおぉ！'
     }
       ])
 })
-// /Applications/MAMP/htdocs/socialNetProj/models
-// /Applications/MAMP/htdocs/socialNetProj/server.js
-// first you require the post model
-var Post = require('./models/post')
-// when the request comes in you build up a new instance of the Post model with the new Post()
+
+//tested code to serve Post in a reverse chronological order
+app.get('api/posts',function(req,res,next){
+  Post.find()
+  .sort('-date')
+  .exec(function(err,posts){
+    if(err){ return next(err) }
+    res.json(posts)
+  })
+})
+
 app.post('/api/posts', function(req,res,next){
     var post = new Post({
       username: req.body.username,
@@ -36,6 +38,16 @@ app.post('/api/posts', function(req,res,next){
       res.json(201,post)
     })
 })
+
+app.get('/', function(req,res){
+  res.sendfile('layouts/posts.html')
+})
+
+// /Applications/MAMP/htdocs/socialNetProj/models
+// /Applications/MAMP/htdocs/socialNetProj/server.js
+// first you require the post model
+var Post = require('./models/post')
+// when the request comes in you build up a new instance of the Post model with the new Post()
 
 
 app.listen(3000,function() {
